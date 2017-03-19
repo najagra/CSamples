f8f5414a850fa7e14e71569126e57ba6672434a3
Najagra <Najagra@FractalV2>
2017-03-19 17:34:39 +0100

    Adding sum()

--- main.c
+++ main.c
@@ -7,16 +7,25 @@
 
 #include <stdio.h>
 
+static void sum(int *a, int *b)
+{
+	*a += *b;
+}
+
 int main( void )
 {
 
     int i = 0;
+    int j = 5;
 
     printf("Get item: ");
 
     fflush(stdout);
 
     scanf("%d", &i);
+    printf("%d\n", i);
+
+    sum(&i, &j);
 
     printf("%d\n", i);
 
