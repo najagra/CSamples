d75e18c367974f871d015f58ce2aebb94098a086
najagra <najagra@fractalV2>
2017-03-26 00:07:24 +0100

    typedef enum

--- src/arrays/simple_arrays.c
+++ src/arrays/simple_arrays.c
@@ -12,23 +12,19 @@
 
 #define ARRAY_LENGTH 10
 
-enum PTYPE { PINT = 1, PCHAR = 2 };
-
-/*
- * Static arrays
- */
+typedef enum _PTYPE { PINT = 1, PCHAR = 2 } PTYPE;
 
 int  s_array_01[ARRAY_LENGTH] = { 1, 2, 3, 4, 5 };
 char s_array_02[ARRAY_LENGTH] = { 'a', 'b' };
 
-static void print_an_array(void *array, int array_type, int array_length)
+static void print_an_array(void *array, PTYPE array_type, int array_length)
 {
 	for ( int i=0 ; i<array_length ; i++ )
 	{
 		switch ( array_type )
 		{
-			case 1: printf("[%d] = %p = %d\n", i, &(( int*)array)[i], *(( int*)array+i) ); break;
-			case 2: printf("[%d] = %p = %c\n", i, &((char*)array)[i], *((char*)array+i) ); break;
+			case PINT : printf("[%d] = %p = %d\n", i, &(( int*)array)[i], *(( int*)array+i) ); break;
+			case PCHAR: printf("[%d] = %p = %c\n", i, &((char*)array)[i], *((char*)array+i) ); break;
 		}
 	}
 }
